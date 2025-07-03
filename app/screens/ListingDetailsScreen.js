edcfca54f055012fc88da70556edd82f92b8e2b0
Leandre <iamshemaleandre@gmail.com>
2025-07-03 11:49:03 +0200

    Added icons

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,47 +1,54 @@
 import React from 'react';
-import { Image,View,StyleSheet } from 'react-native';
+import { Image, View, StyleSheet } from 'react-native';
 import AppText from '../components/AppText';
-
 import colors from '../config/colors';
+
+import ListItem from '../components/ListItem';
 
 function ListingDetailsScreen(props) {
     return (
-
         <View>
-            <Image style={styles.image}  source={require('../assets/jacket.jpg')}/>
+            <Image style={styles.image} source={require('../assets/jacket.jpg')} />
             <View style={styles.detailsContainer}> 
-            <AppText style={styles.title}>Red jacket for sale</AppText>
-            <AppText style={styles.price}>$100</AppText>
+                <AppText style={styles.title}>Red jacket for sale</AppText>
+                <AppText style={styles.price}>$100</AppText>
+                <View style={styles.userContainer}>
 
+                 <ListItem 
+                    image={require("../assets/shema.jpg")}
+                    title="Shema Leandre"
+                    subTitle="5 Listings"
+                />
+
+                </View>
+               
             </View>
-            
         </View>
-       
     );
 }
-
 
 const styles = StyleSheet.create({
     title: {
         fontSize: 24,
         fontWeight: "500",
     },
-    price: {
-        color:colors.secondary,
-        fontWeight:"bold",
-        fontSize:20,
-        marginVertical:10,
+    userContainer:{
+        marginVertical:40,
 
     },
-   detailsContainer:{
+    price: {
+        color: colors.secondary,
+        fontWeight: "bold",
+        fontSize: 20,
+        marginVertical: 10,
+    },
+    detailsContainer: {
         padding: 20,
-
     },
     image: {
         width: "100%",
         height: 300,
-        },
+    },
+});
 
-
-})
 export default ListingDetailsScreen;
--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Image,StyleSheet, View } from 'react-native';
+import {MaterialCommunityIcons} from '@expo/vector-icons'
 
 
 import colors from '../config/colors';
@@ -10,8 +11,12 @@ import colors from '../config/colors';
 function ViewImageScreen(props) {
     return (
         <View style={styles.container}>
-        <View style={styles.closeIcon}></View>
-         <View style={styles.deleteIcon}></View>
+        <View style={styles.closeIcon}>
+            <MaterialCommunityIcons name='close' color="white" size={30}/>
+        </View>
+         <View style={styles.deleteIcon}>
+            <MaterialCommunityIcons  name='trash-can-outline' color="white" size={30}/>
+         </View>
          <Image
         resizeMode='contain'
         style={styles.image}
@@ -32,17 +37,11 @@ const styles = StyleSheet.create({
     },
 
     closeIcon: {
-        width:50,
-        height:50,
-        backgroundColor:colors.primary,
         position:'absolute',
         top:40,
         left:30,
     },
       deleteIcon: {
-        width:50,
-        height:50,
-        backgroundColor:colors.secondary,
         position:'absolute',
         top:40,
         right:30,
