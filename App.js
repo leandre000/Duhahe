6fee7b80c70cebe9833f1501078009bba7cdc3c5
Leandre <iamshemaleandre@gmail.com>
2025-07-03 11:19:36 +0200

    new file added

--- App.js
+++ App.js
@@ -4,22 +4,10 @@ import WelcomeScreen from './app/screens/WelcomeScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 
 import Card from './app/components/Card';
+import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 
 export default function App() {
-  return <View style={{
-    backgroundColor:'#f8f4f4',
-    padding:20,
-    paddingTop:100,
+  return  <ListingDetailsScreen/>
 
-  }}>
-  <Card
-  title="Red jacket for sale"
-  subTitle="$100"
-  image ={require("./app/assets/jacket.jpg")}
-   />
-    
-  </View>
-  
-  ;
 }
 
