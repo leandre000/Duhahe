c74dc562c0458f3a06ca4f9bc7a650bb7c07d961
Leandre <iamshemaleandre@gmail.com>
2025-07-04 15:53:56 +0200

    Added some changes

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -23,7 +23,7 @@ const messages = [
 function MessagesScreen(props) {
   return (
     <Screen style={styles.screen}>
-      <FlatList //hello
+      <FlatList //hel
         data={messages}
         keyExtractor={(message) => message.id.toString()}
         renderItem={({ item }) => (
