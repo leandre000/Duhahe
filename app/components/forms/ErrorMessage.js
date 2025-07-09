8910901655dc51168c27df7cbb0b50acaa4eaab5
Leandre <iamshemaleandre@gmail.com>
2025-07-09 06:07:56 +0200

    modified files

--- app/components/forms/ErrorMessage.js
+++ app/components/forms/ErrorMessage.js
@@ -7,7 +7,7 @@ function ErrorMessage({ error, visible }) {
   if (!visible || !error) return null;
 
   return <AppText style={styles.error}>{error}</AppText>;
-  //work
+  //work here
 }
 
 const styles = StyleSheet.create({
