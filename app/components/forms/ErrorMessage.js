d9e246c8db03767f4342fdf4ceecd8e7fcec62f9
Leandre <iamshemaleandre@gmail.com>
2025-07-09 05:57:41 +0200

    new logic

--- app/components/forms/ErrorMessage.js
+++ app/components/forms/ErrorMessage.js
@@ -7,7 +7,7 @@ function ErrorMessage({ error, visible }) {
   if (!visible || !error) return null;
 
   return <AppText style={styles.error}>{error}</AppText>;
-  
+  //work
 }
 
 const styles = StyleSheet.create({
